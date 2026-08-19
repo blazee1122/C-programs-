a99cec03635f67117a7eaceeb49bc38743dd5f23
Bishwesh Dahal <bishweshdahal11@gmail.com>
2026-08-19 11:54:13 +0545

    Refactor area calculation for square length input

---
+++ area.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    int l, area ;
+    
+    printf("Enter length  of square: ") ;
+    scanf("%d", &l) ;
+    
+    area = l * l ;
+    
+    printf("Area of square = %d\n", area) ;
+    
+    return 0 ;
+}
