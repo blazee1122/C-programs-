66aa1e16c96473c7e50d05aaea1585d4dc56764b
Bishwesh Dahal <bishweshdahal11@gmail.com>
2026-08-19 12:02:18 +0545

    Refactor if-else structure for number classification

--- positive , negative or zero.c
+++ positive , negative or zero.c
@@ -7,10 +7,12 @@ int main()
     printf("Enter a number: ");
     scanf("%d", &num);
     
-    if(num > 0) {
+    if(num > 0) 
+    {
         printf("%d is Positive\n", num);
     }
-    else if(num < 0) {
+    else if(num < 0)
+    {
         printf("%d is Negative\n", num);
     }
     else {
